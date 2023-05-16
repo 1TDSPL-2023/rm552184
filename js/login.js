ee52b59a317731ae9a80a075920a63d3387ff7c0
fatroo <rm552184@fiap.com.br>
2023-05-16 09:44:03 -0300

    calculadora

--- js/login.js
+++ js/login.js
@@ -1,17 +1,23 @@
 
 
 function insereNumero(botao,visor) {
+    if(botao.value =="+" || botao.value =="-" || botao.value =="*" || botao.value =="/"){
+        visor.value += botao.value;
+        console.log(visor.value[-1] =="+"|| visor.value[visor.value.length -1]=="-");
 
-    const meuBotao = document.getElementById(botao);
-    const meuVisor = document.getElementById(visor);
-
-    meuVisor.value += meuBotao.value;
-
+    }else{
+        meuVisor.value += meuBotao.value;
+    }
 }
 function resultado(visor){
     const meuVisor = document.getElementById(visor);    
     let armazenamentoDeNumeros = [];
     armazenamentoDeNumeros = [...visor];
     console.log(armazenamentoDeNumeros);
+try{
+    visor.value = eval(visor.value)
 
+}catch(erro){
+    if(botao.value[visor.value.length] =="+" || botao.value =="-" || botao.value =="*" || botao.value =="/")
+}
 }
